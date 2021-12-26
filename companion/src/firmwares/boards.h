c29310e3e3b28935d42e4695e30e31235b55dfe6
rotorman <risto.koiva@web.de>
2021-12-26 19:57:08 +0100

    Initial rough target for FlySky PL18, based off NV14 target.

--- companion/src/firmwares/boards.h
+++ companion/src/firmwares/boards.h
@@ -66,6 +66,7 @@ namespace Board {
     BOARD_JUMPER_TLITE,
     BOARD_JUMPER_TLITE_F4,
     BOARD_FLYSKY_NV14,
+    BOARD_FLYSKY_PL18,
     BOARD_RADIOMASTER_ZORRO,
     BOARD_JUMPER_TPRO,
     BOARD_BETAFPV_LR3PRO,
@@ -337,6 +338,11 @@ inline bool IS_FLYSKY_NV14(Board::Type board)
   return (board == Board::BOARD_FLYSKY_NV14);
 }
 
+inline bool IS_FLYSKY_PL18(Board::Type board)
+{
+  return (board == Board::BOARD_FLYSKY_PL18);
+}
+
 inline bool IS_TARANIS_XLITE(Board::Type board)
 {
   return board == Board::BOARD_TARANIS_XLITE || board == Board::BOARD_TARANIS_XLITES;
@@ -414,7 +420,7 @@ inline bool IS_FAMILY_HORUS(Board::Type board)
 
 inline bool IS_FAMILY_HORUS_OR_T16(Board::Type board)
 {
-  return IS_FAMILY_HORUS(board) || IS_FAMILY_T16(board) || IS_FLYSKY_NV14(board)/*generally*/;
+  return IS_FAMILY_HORUS(board) || IS_FAMILY_T16(board) || IS_FLYSKY_NV14(board) || IS_FLYSKY_PL18(board);
 }
 
 inline bool IS_HORUS_OR_TARANIS(Board::Type board)
@@ -424,7 +430,7 @@ inline bool IS_HORUS_OR_TARANIS(Board::Type board)
 
 inline bool IS_STM32(Board::Type board)
 {
-  return IS_TARANIS(board) || IS_FAMILY_HORUS_OR_T16(board) || IS_FLYSKY_NV14(board);
+  return IS_TARANIS(board) || IS_FAMILY_HORUS_OR_T16(board) || IS_FLYSKY_NV14(board) || IS_FLYSKY_PL18(board);
 }
 
 inline bool IS_ARM(Board::Type board)
