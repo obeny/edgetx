2dd4a5e2c4d20e938e018fdad1c42b19359b99bc
gagarinlg <malte@langermann.net>
2022-06-12 15:10:27 +0200

    adapt to changes from main

--- radio/src/cli.cpp
+++ radio/src/cli.cpp
@@ -1040,6 +1040,7 @@ int cliSet(const char **argv)
 }
 
 #if defined(ENABLE_SERIAL_PASSTHROUGH)
+#if defined(HARDWARE_INTERNAL_MODULE)
 static etx_module_state_t *spInternalModuleState = nullptr;
 
 static void spInternalModuleTx(uint8_t* buf, uint32_t len)
@@ -1060,6 +1061,7 @@ static const etx_serial_init spIntmoduleSerialInitParams = {
   .polarity = ETX_Pol_Normal,
 };
 
+#endif // HARDWARE_INTERNAL_MODULE
 // TODO: use proper method instead
 extern bool cdcConnected;
 extern uint32_t usbSerialBaudRate(void*);
