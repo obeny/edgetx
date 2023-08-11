ab5076662c719888f1a5262f4a4559e797d85c18
raphaelcoeffic <raphael.coeffic@frafos.com>
2023-08-11 11:14:16 +0200

    Make FUNC_TEST persistent

--- radio/src/dataconstants.h
+++ radio/src/dataconstants.h
@@ -598,10 +598,12 @@ enum Functions {
   FUNC_SET_SCREEN,
 #endif
   FUNC_DISABLE_AUDIO_AMP,
+  FUNC_TEST, // MUST remain last
 #if defined(DEBUG)
-  FUNC_TEST,  // should remain the last before MAX as not added in Companion
-#endif
   FUNC_MAX SKIP
+#else
+  FUNC_MAX SKIP = FUNC_TEST - 1
+#endif
 };
 
 enum TimerModes {
--- radio/src/storage/yaml/yaml_datastructs_x12s.cpp
+++ radio/src/storage/yaml/yaml_datastructs_x12s.cpp
@@ -85,6 +85,7 @@ const struct YamlIdStr enum_Functions[] = {
   {  FUNC_DISABLE_TOUCH, "DISABLE_TOUCH"  },
   {  FUNC_SET_SCREEN, "SET_SCREEN"  },
   {  FUNC_DISABLE_AUDIO_AMP, "DISABLE_AUDIO_AMP"  },
+  {  FUNC_TEST, "TEST"  },
   {  0, NULL  }
 };
 const struct YamlIdStr enum_TimerModes[] = {
